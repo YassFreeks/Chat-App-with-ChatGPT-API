047db035af238ac2fc86b95842e240d6c8d574f6
YassFreeks <y.moutaouakil@hotmail.com>
2023-05-06 16:54:28 +0200

    loading

--- client/src/components/customHeader/index.jsx
+++ client/src/components/customHeader/index.jsx
@@ -1,6 +1,8 @@
+// Import React and icons from the @heroicons/react library
 import React from "react";
 import { ChatBubbleLeftRightIcon, PhoneIcon } from "@heroicons/react/24/solid";
 
+// Define a functional component called CustomerHeader, which receives a prop called 'chat'
 const CustomerHeader = ({ chat }) => {
   return (
     <div className="chat-header">
@@ -20,4 +22,8 @@ const CustomerHeader = ({ chat }) => {
   );
 };
 
+// Export the CustomerHeader component
 export default CustomerHeader;
+
+
+// Overall, this component provides a basic UI for displaying information about a chat, and can be used in a larger chat application to render chat headers.
